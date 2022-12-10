e529c38b7dc350ba9d8e5afc19aa112688c9affd
Allwina <85947960+Allwina@users.noreply.github.com>
2022-12-10 09:46:58 +0530

    Add files via upload
    7-day weather forecast using javascript,HTML and CSS

---
+++ hr.js
@@ -0,0 +1,100 @@
+ // Houlry Forecast
+
+ const apikey="41c159391c307672eeccd8f0cb01798a";
+window.addEventListener("load",()=>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position)=>{
+            let lon= position.coords.longitude;
+            let lat= position.coords.latitude;
+            const url= `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${apikey}`;
+           
+            fetch(url).then((res)=>{
+                return res.json();
+            }).then((data)=>{
+                console.log(data);
+                console.log(new Date().getTime())
+                var dat= new Date(data.dt)
+                console.log(dat.toLocaleString(undefined,'Asia/Delhi'))
+                console.log(new Date().getMinutes())
+                weatherReport(data);
+            })
+        })
+    }
+})
+
+
+function searchByCity(){
+    var place= document.getElementById('input').value;
+    var urlsearch= `http://api.openweathermap.org/data/2.5/weather?q=${place}&` + `appid=${apikey}`;
+
+    fetch(urlsearch).then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        console.log(data);
+        weatherReport(data);
+    })
+    document.getElementById('input').value='';
+}
+
+function weatherReport(data){
+
+    var urlcast= `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
+
+    fetch(urlcast).then((res)=>{
+        return res.json();
+    }).then((forecast)=>{
+        console.log(forecast.city);
+        hourForecast(forecast);
+       
+
+        console.log(data);
+        document.getElementById('city').innerText= data.name + ', '+data.sys.country;
+        console.log(data.name,data.sys.country);
+    
+        console.log(Math.floor(data.main.temp-273));
+        document.getElementById('temperature').innerText= Math.floor(data.main.temp-273)+ ' °C';
+    
+        document.getElementById('clouds').innerText= data.weather[0].description;
+        console.log(data.weather[0].description)
+        
+        let icon1= data.weather[0].icon;
+        let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
+        document.getElementById('img').src=iconurl
+    })
+
+}
+
+function hourForecast(forecast){
+    document.querySelector('.templist').innerHTML=''
+    for (let i = 0; i < 5; i++) {
+
+        var date= new Date(forecast.list[i].dt*1000)
+        console.log((date.toLocaleTimeString(undefined,'Asia/Delhi')).replace(':00',''))
+
+        let hourR=document.createElement('div');
+        hourR.setAttribute('class','next');
+
+        let div= document.createElement('div');
+        let time= document.createElement('p');
+        time.setAttribute('class','time')
+        time.innerText= (date.toLocaleTimeString(undefined,'Asia/Delhi')).replace(':00','');
+
+        let temp= document.createElement('p');
+        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
+        let humid= document.createElement('p');
+        humid.innerText= "Humidity : "+ forecast.list[i].main.humidity + ' %';
+
+
+        div.appendChild(time)
+        div.appendChild(temp)
+        div.appendChild(humid)
+
+        let desc= document.createElement('p');
+        desc.setAttribute('class','desc')
+        desc.innerText= forecast.list[i].weather[0].description;
+
+        hourR.appendChild(div);
+        hourR.appendChild(desc)
+        document.querySelector('.templist').appendChild(hourR);
+}
+}
